1d612c67bc700b46fafcdf173a08ce720269faa0
zhukun <zhukun-analytics@patsnap.com>
2017-05-26 17:51:01 +0800

    list page text right

--- src/components/list/src/main.vue
+++ src/components/list/src/main.vue
@@ -35,7 +35,7 @@
                 </ul>
             </div>
         </slot>
-        <div class="text-center list-page">
+        <div class="text-right list-page">
             <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
             </el-pagination>
         </div>
